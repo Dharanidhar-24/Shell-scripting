2b0f51a17f0bf72ec842d4a747abb112d9f84a9d
Ganjikunta Dharanidhar <gdharani@gdharani-mn7040.linkedin.biz>
2025-11-03 19:50:44 +0530

    arthematic operations

--- arthematic operation.sh
+++ arthematic operation.sh
@@ -6,9 +6,9 @@ c=10
 d=20
 
 echo "Name of the file is : $0"
-echo "SUM: (($a+$b))"
-echo "SUB: (($a-$b))"
-echo "MULTI: (($a*$b))"
-echo "DIV: (($a/$b))"
+echo "SUM: $(($a+$b))"
+echo "SUB: $(($a-$b))"
+echo "MULTI: $(($a*$b))"
+echo "DIV: $(($a/$b))"
 echo
 echo "--------------------------------"
