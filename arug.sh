8e51a3fd70cc2ed395e5f262822ec0cb165a556a
Ganjikunta Dharanidhar <gdharani@gdharani-mn7040.linkedin.biz>
2025-11-03 19:23:14 +0530

    arg

---
+++ arug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a=$1
+b=$2
+
+echo "$a: hi, $b how are you?"
+echo "$b: hi, $a i am fine what about you"
+echo "$a: I am doing good and working in linkedin"
+echo "$b: super cool to this words from you $a"
