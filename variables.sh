ca319fd33d44e11119b31eb1ef86748202714a52
Ganjikunta Dharanidhar <gdharani@gdharani-mn7040.linkedin.biz>
2025-11-03 08:48:34 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+a=dharanidhar
+b=swetha
+
+echo "$a: will you marry me?"
+echo "$b: i am waiting to marry you, my dear $a"
+echo "$a: when we will marry $b"
+echo "$b: frist we will spend some time each other and understand in depth then we will marry"
+echo "$a: comeon lets start today only why to waste all this time"
