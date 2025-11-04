fd7a1c6bb7f1b746909748c756ba433df23059b7
Ganjikunta Dharanidhar <gdharani@gdharani-mn7040.linkedin.biz>
2025-11-04 21:05:08 +0530

    if condition

--- conditions.sh
+++ conditions.sh
@@ -5,7 +5,7 @@ if [ $num -lt 30 ];then
 echo "Giver number $num is less than 30"
 elif [ $num -gt 30 ];then
 echo "Given number $num is greater than 30"
-elif [ $num -eq 30]; then
+elif [ $num -eq 30 ]; then
 echo "Given number $num is equal to 30"
 else
 echo "looks good"
