388fd68e9db9eab0aff7693d1b02e9a5c86a2117
Ganjikunta Dharanidhar <gdharani@gdharani-mn7040.linkedin.biz>
2025-11-04 21:04:06 +0530

    if condition

--- conditions.sh
+++ conditions.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 read -p "Enter the number: " num
-if [$num -lt 30];then
-echo "Giver number $num is lessthan 30"
-elif [$num -gt 30];then
+if [ $num -lt 30 ];then
+echo "Giver number $num is less than 30"
+elif [ $num -gt 30 ];then
 echo "Given number $num is greater than 30"
-else
+elif [ $num -eq 30]; then
 echo "Given number $num is equal to 30"
+else
+echo "looks good"
 fi
 
